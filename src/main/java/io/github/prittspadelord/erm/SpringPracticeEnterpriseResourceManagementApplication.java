af69b87d568908d241c6e5f66b799e483f6c3e45
PrittSpadeLord <pritt1999@gmail.com>
2025-07-24 02:16:49 +0530

    first commit

---
+++ src/main/java/io/github/prittspadelord/erm/SpringPracticeEnterpriseResourceManagementApplication.java
@@ -0,0 +1,7 @@
+package io.github.prittspadelord.erm;
+
+public class SpringPracticeEnterpriseResourceManagementApplication {
+    public static void main(String[] args) {
+        //..
+    }
+}
---
+++ src/main/java/io/github/prittspadelord/erm/config/SpringApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package io.github.prittspadelord.erm.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringApplicationConfiguration {
+}
